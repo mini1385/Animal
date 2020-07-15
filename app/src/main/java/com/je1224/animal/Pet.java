524a2dfb51f5e3e0f8269a0230454449d4ec8679
je1224 <rlawldms1385@gmail.com>
2020-07-15 14:50:18 +0900

    다시

---
+++ app/src/main/java/com/je1224/animal/Pet.java
@@ -0,0 +1,16 @@
+package com.je1224.animal;
+
+public class Pet {
+    String petName;
+    String petGender;
+    String petBirth;
+
+    public Pet() {
+    }
+
+    public Pet(String petName, String petGender, String petBirth) {
+        this.petName = petName;
+        this.petGender = petGender;
+        this.petBirth = petBirth;
+    }
+}
